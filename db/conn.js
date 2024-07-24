e11e31a7782a891a80cf1b32abc3f5ab8aaf4808
JunW62 <sivia1990@gmail.com>
2024-07-24 09:27:23 -0500

    add conn

---
+++ db/conn.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const conn = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connection.once("open", () => {
+      console.log("Connected to MongoDB");
+    });
+  } catch (err) {
+    console.error(
+      `Something went wrong with the connect to the database ${err.message}`
+    );
+  }
+};
+module.export = conn;
